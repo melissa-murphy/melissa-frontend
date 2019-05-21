808ca59c05bb95d1e9e062a8a630d475a52e3d9a
melissa-murphy <melissamurphy.ca@gmail.com>
2019-05-21 09:55:10 -0700

    clinician reducer

---
+++ immunization_tracking/src/reducers/clinicianReducer.js
@@ -0,0 +1,38 @@
+import {
+    FETCHING_CLINICIAN,
+    FETCH_CLINICIAN_SUCCESS,
+    FETCH_CLINICIAN_FAILURE
+  } from '../actions';
+  
+  const initialState = {
+    clinician: [],
+    loading: false,
+    error: ''
+  };
+  
+  function clinicianProfileReducer(state = initialState, action) {
+    console.log(`----------------fetch patient/clinician profile fired`);
+    switch (action.type) {
+      case FETCHING_CLINICIAN:
+        return {
+          ...state,
+          loading: true
+        };
+      case FETCH_CLINICIAN_SUCCESS:
+        return {
+          ...state,
+          friends: action.payload,
+          loading: false
+        };
+      case FETCH_CLINICIAN_FAILURE:
+        return {
+          ...state,
+          error: action.payload,
+          loading: false
+        };
+      default:
+        return state;
+    }
+  }
+  
+  export default clinicianProfileReducer;
