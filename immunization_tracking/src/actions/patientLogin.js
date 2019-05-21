f44bc4089688c1eb8f1f02f4f3d7094a755ac79a
melissa-murphy <melissamurphy.ca@gmail.com>
2019-05-21 14:35:01 -0700

    patient login updates

--- immunization_tracking/src/actions/patientLogin.js
+++ immunization_tracking/src/actions/patientLogin.js
@@ -1,22 +1,3 @@
-// import axios from 'axios';
-// import { axiosAuth } from '../axiosAuth';
-
-// // Login
-// export const LOGGING_IN = 'LOGGING_IN';
-// export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-// export const LOGIN_FAILURE = 'LOGIN_FAILURE';
-
-// export const login = creds => dispatch => {
-//   dispatch({ type: LOGGING_IN });
-//   return axios
-//     .post('https://immunization-tracker-backend.herokuapp.com/login?role=1', creds)
-//     .then(res => {
-//       localStorage.setItem('token', res.data.payload);
-//       dispatch({ type: LOGIN_SUCCESS, payload: res.data.payload });
-//     })
-//     .catch(err => console.log(err));
-// };
-
 import axios from 'axios';
 
 export const LOGIN_START = 'LOGIN_START';
@@ -33,7 +14,8 @@ export const login = creds => dispatch => {
     .then(res => {
       dispatch({ type: LOGIN_SUCCESS, payload: res.data });
       localStorage.setItem('token', res.data.token);
-      localStorage.setItem('data', res.data);
+      localStorage.setItem('patient_id', res.data.patient_id);
+      localStorage.setItem('message', res.data.message)
       const saved = JSON.stringify(res.data);
       localStorage.setItem('data', saved);
       console.log(`---------------------login response` + JSON.stringify(res));
