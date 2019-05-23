a505fc22ec8f4055e83cbf063a3580b9a0754408
melissa-murphy <melissamurphy.ca@gmail.com>
2019-05-22 17:28:22 -0700

    async functional

--- immunization_tracking/src/actions/index.js
+++ immunization_tracking/src/actions/index.js
@@ -7,19 +7,19 @@ export const LOGIN_START = 'LOGIN_START';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAILED = 'LOGIN_FAILED';
 
-export const login = creds => dispatch => {
+export const login = creds => async() => dispatch => {
   console.log(`---------------patient login fired`);
   dispatch({ type: LOGIN_START });
   return axios
     .post(`${URL}/login?role=1`, creds)
     .then(res => {
-      console.log(res);
       localStorage.setItem('token', res.data.token);
       localStorage.setItem('patient_id', res.data.patient_id);
       localStorage.setItem('username', res.data.username);
       localStorage.setItem('message', res.data.message);
       dispatch({ type: LOGIN_SUCCESS, payload: res.data });
       const saved = JSON.stringify(res.data);
+      console.log(`------------------------saved response` + saved)
       localStorage.setItem('data', saved);
       console.log(`---------------------login response` + JSON.stringify(res));
     })
