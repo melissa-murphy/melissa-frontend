0a89f35ce1062eefb6391c8589b00eedf3e561d2
melissa-murphy <melissamurphy.ca@gmail.com>
2019-05-23 23:29:47 -0700

    fleshing reg

--- immunization_tracking/src/components/RegisterPatient.js
+++ immunization_tracking/src/components/RegisterPatient.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import { connect }
 
 import {
@@ -13,36 +13,37 @@ import {
   Button
 } from 'reactstrap';
 
-const RegisterPatient = ({ patients }) => {
-  return (
-    <div>
-      <div className="new-registration">
-        {patients.map(patient => (
-          <Container className='register-new-patient' patient={patient} key={patient.id}>
-            <Card>
-              <CardTitle>Welcome To The Shit Show</CardTitle>
-              <CardSubtitle>Main Dashboard</CardSubtitle>
-              <CardBody>
-                <CardText>name/ID</CardText>
-                <CardText>SS#</CardText>
-                <CardText>Phone#</CardText>
-                <CardText>avatar image</CardText>
-                <CardText>is_kid? (0 | 1)</CardText>
-                <CardText>Username</CardText>
-                <CardText>PW</CardText>
-                <CardText>Email</CardText>
-              </CardBody>
-              <CardFooter>
-                <Button>Kid 1</Button>
-                <Button>Kid 2</Button>
-              </CardFooter>
-            </Card>
-          </Container>
-        ))}
-      </div>
-    </div>
-  );
+class RegisterPatient extends Component {
+  render() {
+    <div className="new-registration">
+      {patients.map(patient => (
+        <Container
+          className="register-new-patient"
+          patient={patient}
+          key={patient.id}
+        >
+          <Card>
+            <CardTitle>Welcome To The Shit Show</CardTitle>
+            <CardSubtitle>Main Dashboard</CardSubtitle>
+            <CardBody>
+              <CardText>name/ID</CardText>
+              <CardText>SS#</CardText>
+              <CardText>Phone#</CardText>
+              <CardText>avatar image</CardText>
+              <CardText>is_kid? (0 | 1)</CardText>
+              <CardText>Username</CardText>
+              <CardText>PW</CardText>
+              <CardText>Email</CardText>
+            </CardBody>
+            <CardFooter>
+              <Button>Kid 1</Button>
+              <Button>Kid 2</Button>
+            </CardFooter>
+          </Card>
+        </Container>
+      ))}
+    </div>;
+  }
 }
 
 export default RegisterPatient;
-
